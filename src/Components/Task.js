a33b1bbfd99f2bf9a88c952481cb47ebaa1f0a2f
mnochtioui <134012404+mnochtioui@users.noreply.github.com>
2023-07-08 22:25:09 +0100

    4rth commit

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -3,18 +3,39 @@ import { useDispatch } from 'react-redux'
 import { checkTask, deleteTask } from '../Redux1/action';
 import { Button } from 'react-bootstrap';
 import ModifTask  from './ModifTask';
+import { useRef } from 'react';
 
 const Task = ({t}) => {
     const dispatch=useDispatch()
+
+    const descRef=useRef(null)
+
+    const handleClick=()=>{
+        descRef.current.contentEditable = "true";
+        descRef.current.focus();
+    }
+
+    const handleBlur = () => {
+        descRef.current.contentEditable = "false";
+         
+    };
+     
 
   return (
     <div key={t.id}  className="d-flex justify-content-between align-items-start" >
             <input className='pull-left' type="checkbox"  onClick={()=>{dispatch(checkTask(t.id))}} defaultChecked={t.isDone} />
             <h5>{t.description}</h5>
-             
+            <span 
+            className="desc"
+                ref={descRef}
+                onClick={handleClick}
+                onBlur={handleBlur}
+            >
+                {t.description}
+                </span>
             <Button  onClick={()=>{dispatch(deleteTask(t.id)) }} >Fassa5</Button>
             <ModifTask props={t} />
-             {/* <Button  onClick={()=>{dispatch(modifTask(t.id)) }} >Editer</Button> */}
+             
     </div>
   )
 }
